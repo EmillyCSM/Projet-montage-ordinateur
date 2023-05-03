a0ce3906b035cfbaf0a0fd8c686550623c2643b7
Malak Bouaker <bouaker.malak@outlook.fr>
2023-05-03 14:51:34 +0200

    ajout du footer / amélioration de l'aspect des formilaires

--- pages/login.php
+++ pages/login.php
@@ -2,8 +2,6 @@
 include_once '../includes/header.php';
 include_once '../includes/footer.php';
 if (!empty($_POST)) {
-
-
     $mail = $_POST['mail'];
     $sql = "SELECT email, password FROM `users` WHERE email = '$mail'";
     $statement = $connection->query($sql);
@@ -16,14 +14,21 @@ if (!empty($_POST)) {
     }
 }
 ?>
-<form method="post">
-    <div class="form-group">
-        <label for="mail">Adresse mail</label>
-        <input type="email" class="form-control" id="mail" name="mail" placeholder="Enter email">
+<form method="post" class="container">
+    <div class="row col-8 col-md-6 col-lg-5 p-3 m-auto gap-4">
+        <div class="form-group">
+            <label for="mail">Adresse mail</label>
+            <input type="email" class="form-control" id="mail" name="mail" placeholder="Enter email">
+        </div>
+        <div class="form-group">
+            <label for="password">Mot de passe</label>
+            <input type="password" class="form-control" name="password" id="password" placeholder="Password">
+        </div>
+        <div>
+    
+</div>
+<button type="submit" class="btn btn-dark m-auto w-25">Valider</button>
+<a id="link_sign_in" href="sign_in.php">pas de compte? s'inscire ici</a>
     </div>
-    <div class="form-group">
-        <label for="password">Password</label>
-        <input type="password" class="form-control" name="password" id="password" placeholder="Password">
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
+
 </form>
--- pages/sign_in.php
+++ pages/sign_in.php
@@ -1,7 +1,5 @@
 <?php
 include_once '../includes/header.php';
-include_once '../includes/footer.php';
-
 
 
 if (!empty($_POST)) {
@@ -14,25 +12,33 @@ if (!empty($_POST)) {
 
   $statement->execute();
 }
-
 ?>
 
-<form method="post">
-  <div class="form-group">
-    <label for="name">Prénom</label>
-    <input type="text" class="form-control" id="name" name="name" placeholder="Prénom">
+<form method="post" class="container">
+  <div class="row col-8 col-md-6 col-lg-5 p-3 m-auto gap-4">
+    <div class="form-group">
+      <label for="name">Prénom</label>
+      <input type="text" class="form-control" id="name" name="name" placeholder="Prénom">
+    </div>
+    <div class="form-group">
+      <label for="mail">Adresse mail</label>
+      <input type="email" class="form-control" id="mail" name="mail" placeholder="email">
+    </div>
+    <div class="form-group">
+      <label for="password">Mot de passe</label>
+      <input type="password" class="form-control" name="password" id="password" placeholder="mot de passe">
+    </div>
+    <div class="form-group">
+      <label for="status">Votre status</label>
+      <select name="status" id="status" class="form-select">
+        <option value="1">Concepteur</option>
+        <option value="0">Monteur</option>
+      </select>
+    </div>
+    <button type="submit" class="btn btn-dark w-25 m-auto">Valider</button>
   </div>
-  <div class="form-group">
-    <label for="mail">Adresse mail</label>
-    <input type="email" class="form-control" id="mail" name="mail" placeholder="email">
-  </div>
-  <select name="status" id="status">
-    <option value="1">Concepteur</option>
-    <option value="0">Monteur</option>
-  </select>
-  <div class="form-group">
-    <label for="password">Mot de passe</label>
-    <input type="password" class="form-control" name="password" id="password" placeholder="mot de passe">
-  </div>
-  <button type="submit" class="btn btn-primary">Valider</button>
 </form>
+
+<?php
+include_once '../includes/footer.php';
+?>
