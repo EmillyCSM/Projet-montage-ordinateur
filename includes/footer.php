f38e6c371e2c9d1093a9a0af7a93d5f29a453c04
Malak Bouaker <bouaker.malak@outlook.fr>
2023-05-04 09:47:02 +0200

    ajout dossier class

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 <footer class="text-center">
-    Malak_Emilly©HumanBooster2023
+    Malak_gitEmilly©HumanBooster2023
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
     crossorigin="anonymous"></script>
