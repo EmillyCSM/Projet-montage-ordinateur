6d8b57751a1dc37eafc3bf6bf2c64af146790c0d
Rémi Jarjat <remi@dreeckan.net>
2023-05-12 11:26:43 +0200

    38 - Typos

--- pages/logout.php
+++ pages/logout.php
@@ -3,4 +3,4 @@ session_destroy();
 // Dans logout.php :
 // On transmet un paramètre GET pour noter que l'utilisateur est déconnecté
 
- header(Location: ?logout=success);
+ header("Location: ?logout=success");
